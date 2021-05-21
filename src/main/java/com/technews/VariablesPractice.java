4c6c1c8f0087f6d53d070ca2d473753eb930429b
Russ <ocjade@comcast.net>
2021-05-20 20:37:34 -0600

    User, Post and Comment Models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int number = 45;
+        double decimal = .5;
+        double sum = number + decimal;
+
+        String name = "Russ";
+        String nameAge = name + sum;
+
+        System.out.println(nameAge);
+    }
+}
