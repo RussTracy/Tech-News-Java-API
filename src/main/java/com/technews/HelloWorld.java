b495ab6f2b19c6939465e9275379d72bb792f4a4
Russ <ocjade@comcast.net>
2021-05-28 22:26:28 -0600

    Quick Update

--- src/main/java/com/technews/HelloWorld.java
+++ src/main/java/com/technews/HelloWorld.java
@@ -3,5 +3,6 @@ package com.technews;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is my cool Java app!");
     }
 }
